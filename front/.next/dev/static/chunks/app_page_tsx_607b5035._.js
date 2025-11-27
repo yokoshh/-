776c10a76d77d1b30fbccdec875f297eab745e0b
Mars-Lucifer <nikitarybalko897@gmail.com>
2025-11-28 00:05:42 +0300

    delete dev

--- front/.next/dev/static/chunks/app_page_tsx_607b5035._.js
+++
@@ -1,7 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/_a1b13bc1._.js"
-],
-    source: "dynamic"
-});
